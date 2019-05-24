2a5af17983c51af835bfc661e8d1036ea0ae3f5b
Fabio Lombardo <lombax85@gmail.com>
2019-05-24 21:41:07 +0200

    fix benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -6,10 +6,10 @@ export CURRENT_DATE=`date "+%Y-%m-%d_%H-%M"`
 
 for i in {1..1000}
 do
-    (time $(curl -s http://localhost:81/js?EXAMPLEQUERYSTRING > /dev/null)) >> ./results/`echo $CURRENT_DATE`_static.txt 2>&1
+    (time $(curl -s http://localhost:81/js?EXAMPLEQUERYSTRING > /dev/null)) >> ./results/`echo $CURRENT_DATE`_javascript.txt 2>&1
 done
 
 for i in {1..1000}
 do
-    (time $(curl -s http://localhost:81/static/static_file.txt?EXAMPLEQUERYSTRING > /dev/null)) >> ./results/`echo $CURRENT_DATE`_javascript.txt 2>&1
+    (time $(curl -s http://localhost:81/static/static_file.txt?EXAMPLEQUERYSTRING > /dev/null)) >> ./results/`echo $CURRENT_DATE`_static.txt 2>&1
 done
