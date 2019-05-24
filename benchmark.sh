806d5331be00067d14d39911708116aeb70dc641
Fabio Lombardo <lombax85@gmail.com>
2019-05-24 18:24:36 +0200

    fix date format

--- benchmark.sh
+++ benchmark.sh
@@ -2,12 +2,14 @@
 
 TIMEFORMAT=%R
 
+export CURRENT_DATE=`date "+%Y-%m-%d_%H-%M"`
+
 for i in {1..1000}
 do
-    (time $(curl -s http://localhost:81/js?EXAMPLEQUERYSTRING > /dev/null)) >> ./results/static`date "+%Y%m%d-%H-%M"`.txt 2>&1
+    (time $(curl -s http://localhost:81/js?EXAMPLEQUERYSTRING > /dev/null)) >> ./results/`echo $CURRENT_DATE`_static.txt 2>&1
 done
 
 for i in {1..1000}
 do
-    (time $(curl -s http://localhost:81/static/static_file.txt?EXAMPLEQUERYSTRING > /dev/null)) >> ./results/javascript`date "+%Y%m%d-%H-%M"`.txt 2>&1
+    (time $(curl -s http://localhost:81/static/static_file.txt?EXAMPLEQUERYSTRING > /dev/null)) >> ./results/`echo $CURRENT_DATE`_javascript.txt 2>&1
 done
